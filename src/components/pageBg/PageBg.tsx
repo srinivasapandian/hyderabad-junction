1de6ccb299c22ce30b52eadc43b1ffd52faf23db
Prince <prince.t@maghil.com>
2026-05-08 11:48:19 +0530

    gradient-removed

--- src/components/pageBg/PageBg.tsx
+++ src/components/pageBg/PageBg.tsx
@@ -9,9 +9,8 @@ interface PageBgProps {
 export default function PageBg({ className = '', children }: PageBgProps) {
   return (
     <div className={`pg-bg${className ? ` ${className}` : ''}`}>
-      <div className="pg-blob" style={{ top: '-80px',  left:  '-120px' }} />
-      <div className="pg-blob" style={{ top:  '45vh',  right: '-150px' }} />
-      <div className="pg-blob" style={{ top:  '90vh',  left:  '-120px' }} />
+      {/* Blobs removed to eliminate the dark gradient effect */}
+
 
       <img src={designImg} className="pg-deco pg-deco--left"  aria-hidden="true" alt="" />
       <img src={designImg} className="pg-deco pg-deco--right" aria-hidden="true" alt="" />
